429de8feb24ba4ab47da5d0657b10027f031065c
Iman Zarrabian <iman@omts.fr>
2018-06-25 17:35:04 +0200

    Hello staging

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,7 +6,7 @@ public func routes(_ router: Router) throws {
 
     // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello Master!"
+        return "Hello Staging!"
     }
 
     let acronymsController = AcronymsController()
